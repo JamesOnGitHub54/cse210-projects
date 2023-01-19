c113564f16c766de8b49df36246a7b0bca24ab87
James <82992696+JamesOnGitHub54@users.noreply.github.com>
2023-01-18 19:19:36 -0700

    Completed Learning02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,27 @@
 using System;
 
-class Program
+public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Superstar";
+        job2._company = "World Wrestling Entertainment";
+        job2._startYear = 1990;
+        job2._endYear = 2020;
+
+        Resume resume1 = new Resume();
+        resume1._name = "Mark Calaway";
+
+        resume1._Jobs.Add(job1);
+        resume1._Jobs.Add(job2);
+
+        resume1.Display();
     }
 }
